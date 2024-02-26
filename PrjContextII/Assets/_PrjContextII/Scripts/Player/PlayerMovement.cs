b72f4ea38e782e05c1d22d6187d4eb44cde26cd9
Adam <Fireworksanimations21@gmail.com>
2024-02-26 01:20:36 +0100

    speler kan weg bewegen van de pc
    en daarna ook vrij rond bewegen.

--- PrjContextII/Assets/_PrjContextII/Scripts/Player/PlayerMovement.cs
+++ PrjContextII/Assets/_PrjContextII/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 public class PlayerMovement : State
 {
@@ -66,37 +67,75 @@ public class PlayerMovementFree : State
 {
     public Rigidbody rb;
     public Animator anim;
+    private Camera MainCam;
     private PlayerStateHandler PlayerFSM;
-    private float speed = 10f;
+    private float speed = 2f;
 
-    public PlayerMovementFree(Rigidbody _rb, Animator _anim, PlayerStateHandler _fsm)
+    //voor rond kijken
+    private float sensX = 200, sensY = 200, xRotation = 1, yRotation = 1;
+    // private Transform Orientation;
+
+    public PlayerMovementFree(Rigidbody _rb, Animator _anim, PlayerStateHandler _fsm, Camera _mainCam)
     {
         anim = _anim;
         rb = _rb;
         PlayerFSM = _fsm;
+        MainCam = _mainCam;
     }
 
     public override void OnEnter()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         // Debug.Log("test");
         anim.enabled = false;
     }
     public override void OnUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Debug.Log(moveHorizontal);
-        float moveVertical = Input.GetAxis("Vertical");
+        //move
+        movement();
 
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        Debug.Log(movement * speed);
-
-        // rb.AddForce(movement * speed);
-        rb.velocity = movement * speed;
+        //look
+        ReadMouseInput();
     }
     public override void OnExit()
     {
         base.OnExit();
         anim.enabled = true;
+    }
+
+    public void ReadMouseInput()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        //roteer de camera
+        MainCam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        // Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+    public void movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Krijg de richting vooruit en rechts gebaseerd op de camera's oriëntatie
+        Vector3 forward = MainCam.transform.forward;
+        Vector3 right = MainCam.transform.right;
+
+        // Zorg ervoor dat de beweging niet beïnvloed wordt door de hoogte van de camera
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 movement = (forward * moveVertical + right * moveHorizontal) * speed;
+        // Debug.Log(movement * speed);
+
+        // Pas de snelheid van de Rigidbody aan op basis van de berekende richting
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z); // Behoud de huidige verticale snelheid (bijvoorbeeld voor zwaartekracht)
     }
 }
 
