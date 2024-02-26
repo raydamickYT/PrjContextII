b215d973447a93e67fbe37dbb953eae6e89837d0
Adam <Fireworksanimations21@gmail.com>
2024-02-26 23:10:26 +0100

    kleine tests

--- PrjContextII/Assets/_PrjContextII/Scripts/PC's/ScreenInteraction.cs
+++ PrjContextII/Assets/_PrjContextII/Scripts/PC's/ScreenInteraction.cs
@@ -39,27 +39,28 @@ public class ScreenInteraction : MonoBehaviour
             RaycastFromScreenCamera(texturePos, screenCamera);
         }
     }
-Vector2 CalculateTexturePosition(Vector3 hitPoint, GameObject screenObject)
-{
-    // krijg de afmetingen van het schermobject
-    MeshRenderer screenMeshRenderer = screenObject.GetComponent<MeshRenderer>();
-    if (screenMeshRenderer == null) return Vector2.zero;
+    Vector2 CalculateTexturePosition(Vector3 hitPoint, GameObject screenObject)
+    {
+        // krijg de afmetingen van het schermobject
+        MeshRenderer screenMeshRenderer = screenObject.GetComponent<MeshRenderer>();
+        if (screenMeshRenderer == null) return Vector2.zero;
 
-    Vector3 screenBounds = screenMeshRenderer.bounds.size;
-    Debug.Log(screenMeshRenderer.bounds.size);
-    
-    // Bereken de lokale positie van de hit binnen het schermobject
-    Vector3 localHitPoint = screenObject.transform.InverseTransformPoint(hitPoint);
-    float localX = (localHitPoint.x + screenBounds.x / 2) / screenBounds.x;
-    float localY = (localHitPoint.y + screenBounds.y / 2) / screenBounds.y;
+        Vector3 screenBounds = new Vector3(100,100,0);
+        Debug.Log(screenMeshRenderer.bounds.size);
 
-    // Zet deze lokale positie om naar een positie op de Render Texture
-    return new Vector2(localX * screenCamera.pixelWidth, localY * screenCamera.pixelHeight);
-}
+        // Bereken de lokale positie van de hit binnen het schermobject
+        Vector3 localHitPoint = screenObject.transform.InverseTransformPoint(hitPoint);
+        float localX = (localHitPoint.x + screenBounds.x / 2) / screenBounds.x;
+        float localY = (localHitPoint.y + screenBounds.y / 2) / screenBounds.y;
+
+        // Zet deze lokale positie om naar een positie op de Render Texture
+        return new Vector2(localX * screenCamera.pixelWidth, localY * screenCamera.pixelHeight);
+    }
     void RaycastFromScreenCamera(Vector2 screenPosition, Camera screenCamera)
     {
         Ray ray = screenCamera.ScreenPointToRay(screenPosition);
-        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red, 200.0f);
+        Debug.Log(screenPosition);
+        Debug.DrawLine(ray.origin, ray.direction * 100, Color.red, 200.0f);
 
         // Log the start position of the ray
         // Debug.Log("Ray origin: " + ray.origin);
@@ -69,7 +70,7 @@ Vector2 CalculateTexturePosition(Vector3 hitPoint, GameObject screenObject)
         if (Physics.Raycast(ray, out hit, 1000f))
         {
             // Log hit information
-            // Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+            Debug.Log("Raycast hit: " + hit.transform.position);
         }
         else
         {
