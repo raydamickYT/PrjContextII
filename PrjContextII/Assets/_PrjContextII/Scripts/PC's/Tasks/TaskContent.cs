9cd5bfa334f05797fadb76741ed791365dba13fb
Adam <Fireworksanimations21@gmail.com>
2024-03-10 15:13:43 +0100

    Task Screen verbetert

--- PrjContextII/Assets/_PrjContextII/Scripts/PC's/Tasks/TaskContent.cs
+++ PrjContextII/Assets/_PrjContextII/Scripts/PC's/Tasks/TaskContent.cs
@@ -29,97 +29,68 @@ public class TaskContent : MonoBehaviour
         }
         else
         {
-            btn.onClick.AddListener(() => OpenTask());
+            btn.onClick.AddListener(() => SwitchToTaskContentState());
         }
 
     }
-    private void OnEnable()
+
+    // // Deze functie toont de inhoud van de mail
+    // public void OpenTask()
+    // {
+    //     TaskTekst = choiceManager.ChoicesOfToday[choiceManager.CurrentChoiceIndex].choiceText;
+
+    //     // Controleer of er al een instantie bestaat en vernietig deze indien nodig
+    //     if (MailWipeSingletons.Instance.MailIsShowing)
+    //     {
+    //         // MailWipeSingletons.Instance.TriggerAction();
+    //         OpenTask();
+    //     }
+    //     else if (!taskIsShowing)
+    //     {
+    //         // Debug.Log("Mail geopend met ID: ");
+    //         // Maak een instantie van de mail ScrollView prefab
+    //         GameObject taskInstance = Instantiate(TaskPrefab, transform.parent.parent, false);
+    //         TaskInstanceBackup = taskInstance;
+
+    //         taskInstance.transform.position += new Vector3(0.06f, 0.08f, -0.03f);
+    //         taskInstance.transform.localScale += new Vector3(500, 500);
+    //         taskInstance.transform.SetParent(transform.parent.parent.parent); //niet de netste manier om de parent te veranderen...
+
+    //         if (taskInstance.transform.Find("TaskContent").GetComponent<Text>() != null)
+    //         {
+
+    //             Text textComponent = taskInstance.transform.Find("TaskContent").GetComponent<Text>();
+
+    //             textComponent.text = TaskTekst;
+
+    //             // Pas de grootte van het Text-component aan op basis van de tekstlengte
+    //             float preferredHeight = textComponent.preferredHeight;
+
+    //             // Pas de grootte van de RectTransform aan op basis van de preferredWidth en preferredHeight
+    //             RectTransform textRectTransform = textComponent.rectTransform;
+    //             textRectTransform.sizeDelta = new Vector2(textRectTransform.sizeDelta.x, preferredHeight);
+
+    //             // Pas de ankerpositie aan zodat de tekst aan de bovenkant blijft vastzitten
+    //             textRectTransform.pivot = new Vector2(0.5f, 1f);
+    //             textRectTransform.anchorMin = new Vector2(0.5f, 1f);
+    //             textRectTransform.anchorMax = new Vector2(0.5f, 1f);
+
+    //             //bool flag
+    //             taskIsShowing = true;
+    //             MailWipeSingletons.Instance.MailIsShowing = taskIsShowing;
+
+    //         }
+    //         else
+    //         {
+    //             Debug.LogWarning("geen tekst object gevonden in mail prefab");
+    //         }
+
+    //     }
+    // }
+
+    private void SwitchToTaskContentState()
     {
-        // MailWipeSingletons.Instance.SubscribeToAction(CloseMail);
-    }
-
-    private void OnDisable()
-    {
-        // MailWipeSingletons.Instance.UnsubscribeFromAction(CloseMail);
-    }
-
-    void Update()
-    {
-        if (taskIsShowing)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                Yes();
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                No();
-            }
-        }
-    }
-
-    // Deze functie toont de inhoud van de mail
-    public void OpenTask()
-    {
-        TaskTekst = choiceManager.ChoicesOfToday[choiceManager.CurrentChoiceIndex].choiceText;
-
-        // Controleer of er al een instantie bestaat en vernietig deze indien nodig
-        if (MailWipeSingletons.Instance.MailIsShowing)
-        {
-            MailWipeSingletons.Instance.TriggerAction();
-            OpenTask();
-        }
-        else if (!taskIsShowing)
-        {
-            // Debug.Log("Mail geopend met ID: ");
-            // Maak een instantie van de mail ScrollView prefab
-            GameObject taskInstance = Instantiate(TaskPrefab, transform.parent.parent, false);
-            TaskInstanceBackup = taskInstance;
-
-            taskInstance.transform.position += new Vector3(0.06f, 0.08f, -0.03f);
-            taskInstance.transform.localScale += new Vector3(500, 500);
-            taskInstance.transform.SetParent(transform.parent.parent.parent); //niet de netste manier om de parent te veranderen...
-
-            if (taskInstance.transform.Find("TaskContent").GetComponent<Text>() != null)
-            {
-
-                Text textComponent = taskInstance.transform.Find("TaskContent").GetComponent<Text>();
-
-                textComponent.text = TaskTekst;
-
-                // Pas de grootte van het Text-component aan op basis van de tekstlengte
-                float preferredHeight = textComponent.preferredHeight;
-
-                // Pas de grootte van de RectTransform aan op basis van de preferredWidth en preferredHeight
-                RectTransform textRectTransform = textComponent.rectTransform;
-                textRectTransform.sizeDelta = new Vector2(textRectTransform.sizeDelta.x, preferredHeight);
-
-                // Pas de ankerpositie aan zodat de tekst aan de bovenkant blijft vastzitten
-                textRectTransform.pivot = new Vector2(0.5f, 1f);
-                textRectTransform.anchorMin = new Vector2(0.5f, 1f);
-                textRectTransform.anchorMax = new Vector2(0.5f, 1f);
-
-                //bool flag
-                taskIsShowing = true;
-                MailWipeSingletons.Instance.MailIsShowing = taskIsShowing;
-
-            }
-            else
-            {
-                Debug.LogWarning("geen tekst object gevonden in mail prefab");
-            }
-
-        }
-    }
-
-    private void Yes()
-    {
-        choiceManager.MakeChoice(true);
-    }
-
-    private void No()
-    {
-        choiceManager.MakeChoice(false);
+        ComputerManager.instance.SwitchComputerState(typeof(TaskContentScreenState));
     }
 
     public void CloseMail()
